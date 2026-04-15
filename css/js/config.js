2359ae0c6ac21fe0a76c9c676e8a52a87b3f5c95
sreerajendran777-pixel <sreerajendran777@gmail.com>
2026-04-15 21:26:52 +0530

    Update config.js

--- css/js/config.js
+++ css/js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 const CONFIG = {
-    API_KEY: "YOUR_API_KEY_HERE",     // ← You will replace this later
+    API_KEY: "a5e096823a98dbe5d33aa82d4320771f",     // ← You will replace this later
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     DEFAULT_CITY: "Chennai",          // Change to your city if you want
     UNITS: "metric"                   // metric = Celsius, imperial = Fahrenheit
